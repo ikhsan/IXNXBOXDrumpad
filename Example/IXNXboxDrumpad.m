5a44e68a3c173c7fff2aa1b7d03ecc33c3c49408
Ikhsan Assaat <ixnixnixn@yahoo.com>
2014-01-14 18:20:06 +0000

    removed submodule hidapi

---
+++ Example/IXNXboxDrumpad.m
@@ -0,0 +1,13 @@
+//
+//  IXNXboxDrumpad.m
+//  Example
+//
+//  Created by Ikhsan Assaat on 1/14/14.
+//  Copyright (c) 2014 Ikhsan Assaat. All rights reserved.
+//
+
+#import "IXNXboxDrumpad.h"
+
+@implementation IXNXboxDrumpad
+
+@end
---
+++ IXNXboxDrumpad/IXNXboxDrumpad.h
@@ -0,0 +1,13 @@
+//
+//  IXNXboxDrumpad.h
+//  Example
+//
+//  Created by Ikhsan Assaat on 1/14/14.
+//  Copyright (c) 2014 Ikhsan Assaat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IXNXboxDrumpad : NSObject
+
+@end
